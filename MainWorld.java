92b2f4c565e420cebd4c5cc4f4c284bc075b754d
zachary <52474695+aleph-bull@users.noreply.github.com>
2025-04-02 12:00:48 -0400

    Update MainWorld.java
    Changed world dimensions.

--- MainWorld.java
+++ MainWorld.java
@@ -16,6 +16,6 @@ public class MainWorld extends World
     public MainWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1024, 800, 1); 
     }
 }
