e714cbfecbad0f7bce85b451131e8184fa1cdc84
zachary <52474695+aleph-bull@users.noreply.github.com>
2025-04-04 08:58:14 -0400

    Update MainWorld.java

--- MainWorld.java
+++ MainWorld.java
@@ -1,9 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Hi guys!
- * Asonfin
- * hello walalalal bardilo crocodilo crocodilo
+ * 
  */
 public class MainWorld extends World
 {
