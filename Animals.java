2a03698cd3fe956593d4f2a2b403c66f5d1b1325
zachary <52474695+aleph-bull@users.noreply.github.com>
2025-04-04 09:12:14 -0400

    Update Animals.java

--- Animals.java
+++ Animals.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Animals extends Actor
+public class Animals extends SuperSmoothMover
 {
     /**
      * Act - do whatever the Animals wants to do. This method is called whenever
