fd460cba909f7f951bab0f8c85c4b9e74c1482a4
Stephanie Xia <xiasyl88@gmail.com>
2025-04-07 09:18:58 -0400

    Changed
    changed mainworld for relative spawn

--- MainWorld.java
+++ MainWorld.java
@@ -17,6 +17,32 @@ public class MainWorld extends World
         super(1024, 800, 1); 
         background = new GreenfootImage("emptyBackground.png");
         setBackground (background);
+<<<<<<< Updated upstream
         
+=======
+
+        relativeCountdown = 10;
+        relativeMinCountdown = 500; 
+
+    }
+
+    public void act()
+    {
+        spawnRelative(); 
+    }
+
+    public void spawnRelative()
+    {
+        if(relativeCountdown>0)
+        {
+            relativeCountdown --;
+        }
+        else
+        {
+            relative = new Relative();
+            addObject(relative, 50, 200); 
+            relativeCountdown = relativeMinCountdown + Greenfoot.getRandomNumber(200); 
+        }
+>>>>>>> Stashed changes
     }
 }
