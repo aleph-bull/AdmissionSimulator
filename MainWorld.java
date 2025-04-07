718c278c9a47b773fe232bd80678ef9fe8a2e659
Daniel <103289261+DanisLol@users.noreply.github.com>
2025-04-07 09:20:41 -0400

    Add External class and remove Pets and Relative

--- MainWorld.java
+++ MainWorld.java
@@ -11,12 +11,39 @@ public class MainWorld extends World
      * 
      */
     private GreenfootImage background;
-    
+
+    private int relativeCountdown;
+    private int relativeMinCountdown; 
+    private External external;
+
     public MainWorld()
     {    
         super(1024, 800, 1); 
         background = new GreenfootImage("emptyBackground.png");
         setBackground (background);
-        
+
+        relativeCountdown = 500;
+        relativeMinCountdown = 500; 
+
+    }
+
+    public void act()
+    {
+        spawnRelative(); 
+    }
+
+    public void spawnRelative()
+    {
+        if(relativeCountdown>0)
+        {
+            relativeCountdown --;
+        }
+        else
+        {
+            external = new External();
+            addObject(external, 100, 200); 
+            relativeCountdown = relativeMinCountdown + Greenfoot.getRandomNumber(200); 
+        }
+
     }
 }
