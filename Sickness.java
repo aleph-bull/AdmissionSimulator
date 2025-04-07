33ea1242d4b30646124816cfceb022b2f5dc17f5
angxw <angelawang408@gmail.com>
2025-04-07 08:26:25 -0400

    Added classes

--- Sickness.java
+++ Sickness.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Pet here.
+ * Write a description of class Sickness here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Pet extends Relative
+public class Sickness extends Effect
 {
     /**
-     * Act - do whatever the Pet wants to do. This method is called whenever
+     * Act - do whatever the Sickness wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
