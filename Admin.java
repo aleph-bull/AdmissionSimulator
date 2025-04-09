87ff6236e0ceee853c7b935a92a2263a008dd50d
Daniel <103289261+DanisLol@users.noreply.github.com>
2025-04-09 11:56:20 -0400

    Update Admin.java
    Not fully implemented methods

--- Admin.java
+++ Admin.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Admin here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @ Daniel Wang
+ * @ 1.1.1
  */
 public class Admin extends Animals
 {
@@ -13,20 +13,43 @@ public class Admin extends Animals
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
-    public Admin(){
+    private int requirement;
+    private boolean mood;
+    private String uni;
+    private int acceptanceR;
+    
+    
+    public Admin(int requirement,int acceptanceR,boolean mood, String uni){
+        this.requirement = requirement;
+        this.acceptanceR = acceptanceR;
+        this.mood = mood;
+        this.uni = uni;
+        if(!mood){
+            requirement+=2;
+            acceptanceR-=5;
+        }
         
     }
     
     public void act()
     {
-        // Add your action code here.
+        
     }
     
-    public boolean compareStats(){
+    /*
+    public void sendLetter(){
+        
+    }
+    */
+    
+    public boolean compareStats(Student student1){
         boolean result = false;
         //Compares the university standards and the student's stats to consider if 
         //the student should be acceped or rejected
-        // 
+        if(student1.getGpa() >= this.requirement){
+            result = true;
+        }
         return result; 
     }
+    
 }
