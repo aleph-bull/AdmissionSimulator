40189bd770f0a2716873d09b56abca4f95c84639
Stephanie Xia <xiasyl88@gmail.com>
2025-04-17 09:56:29 -0400

    Updated settings world + graphics mc phone done

--- SettingsWorldGeneral.java
+++ SettingsWorldGeneral.java
@@ -1,91 +1,49 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class SettingWorld here.
+ * Write a description of class SettingWorldGeneral here.
  * 
- * @author Stephanie Xia
+ * @author (your name) 
+ * @version (a version number or a date)
  */
-public class SettingsWorldGeneral extends World
+public class SettingsWorldGeneral extends SettingsWorld
 {
 
     /**
-     * Constructor for objects of class SettingWorld.
+     * Constructor for objects of class SettingWorldGeneral.
      * 
      */
-    private Button student1Left; 
-    private Button student1Right;
-    private int student1ImageNumber; 
-    private SettingsImages student1; 
-
-    private Button student2Left; 
-    private Button student2Right;
-    private int student2ImageNumber; 
-    private SettingsImages student2; 
-
-    private Button universityLeft; 
-    private Button universityRight;
-    private int universityImageNumber; 
-    private SettingsImages university; 
-
-    private Cursor cursor;
-    private GreenfootImage background;
-    
-    private Button next; 
-    private Button back;
+    private SettingsWorld.StatChooseImage student1, student2, university;
+    private int student1Number, student2Number, universityNumber; 
 
     public SettingsWorldGeneral()
     {    
-        super(1024, 800, 1); 
         background = new GreenfootImage("SettingsPg1.png"); 
         setBackground(background); 
 
-        //cursor object for hoverable buttons
-        cursor = new Cursor();
-        addObject(cursor, 0, 0);
-        //button
-        student1 = new SettingsImages(); 
-        student1.setImageFile("bob_run18.png"); 
-        addObject(student1, 250, 290); 
-        student1Left = new Button(cursor, true); 
-        student1Right = new Button(cursor, false);
-        addObject(student1Left, 100, 290); 
-        addObject(student1Right, 400, 290); 
 
-        student2 = new SettingsImages();
-        student2.setImageFile("bob_run18.png"); 
-        addObject(student2, 250, 550); 
-        student2Left = new Button(cursor, true); 
-        student2Right = new Button(cursor, false);
-        addObject(student2Left, 100, 550); 
-        addObject(student2Right, 400, 550); 
-
-        university = new SettingsImages();
-        university.setImageFile("universityofcatmeme.png"); 
-        addObject(university, 725, 410); 
-        universityLeft = new Button(cursor, true); 
-        universityRight = new Button(cursor, false);
-        addObject(universityLeft, 500, 550); 
-        addObject(universityRight, 942, 550); 
-        
         next = new Button (cursor, false);
         addObject(next, 945, 670);
         back = new Button (cursor, true); 
         addObject(back, 100, 670); 
-        
 
-        setBackground(background); 
+        student1Number = 0;
+        student1 = new SettingsWorld.StatChooseImage(250, 100, 400, 290, student1Number); 
+        student2Number = 0;
+        student2 = new SettingsWorld.StatChooseImage(250, 100, 400, 550, student2Number); 
+        universityNumber = 0; 
+        university = new SettingsWorld.StatChooseImage(725, 500, 942, 550, 410, universityNumber); 
+
     }
-
     public void act()
     {
-        student1Choose(); 
-        student2Choose(); 
-        universityChoose(); 
+        student1.choose("bob_run18.png", "Amelia_run1.png", "Alex_run1.png"); 
+        student2.choose("bob_run18.png", "Amelia_run1.png", "Alex_run1.png"); 
+        university.choose("universityofcatmeme.png", "universityofbigmac.png", "universityofducksong.png", "universityoflebron.png"); 
         backWorld();
         nextWorld();
-
     }
-    
+
     public void backWorld()
     {
         if (Greenfoot.mouseClicked(back))
@@ -93,6 +51,7 @@ public class SettingsWorldGeneral extends World
             Greenfoot.setWorld(new InstructionWorld()); 
         }
     }
+
     public void nextWorld()
     {
         if (Greenfoot.mouseClicked(next))
@@ -100,83 +59,5 @@ public class SettingsWorldGeneral extends World
             Greenfoot.setWorld(new SettingsWorldS1Stats()); 
         }
     }
-
-    public void student1Choose()
-    {
-        if (Greenfoot.mouseClicked(student1Right))
-        {
-            student1ImageNumber++;    
-        }
-        else if (Greenfoot.mouseClicked(student1Left))
-        {
-            student1ImageNumber--; 
-        }
-        if(student1ImageNumber%3==0)
-        {
-            student1.setImageFile("bob_run18.png"); 
-        }
-        else if (student1ImageNumber%3==1 || student1ImageNumber%3==-1)
-        {
-            student1.setImageFile("mirror.png"); 
-        }
-        else if (student1ImageNumber%3 == 2 || student1ImageNumber%3==-2)
-        {
-            student1.setImageFile("germ1.png"); 
-        }
-
-    }
-
-    public void student2Choose()
-    {
-        if (Greenfoot.mouseClicked(student2Right))
-        {
-            student2ImageNumber++;    
-        }
-        else if (Greenfoot.mouseClicked(student2Left))
-        {
-            student2ImageNumber--; 
-        }
-        if(student2ImageNumber%3==0)
-        {
-            student2.setImageFile("bob_run18.png"); 
-        }
-        else if (student2ImageNumber%3==1 || student2ImageNumber%3==-1)
-        {
-            student2.setImageFile("mirror.png"); 
-        }
-        else if (student2ImageNumber%3 == 2 || student2ImageNumber%3==-2)
-        {
-            student2.setImageFile("germ1.png"); 
-        }
-
-    }
-
-    public void universityChoose()
-    {
-        if (Greenfoot.mouseClicked(universityRight))
-        {
-            universityImageNumber++;    
-        }
-        else if (Greenfoot.mouseClicked(universityLeft))
-        {
-            universityImageNumber--; 
-        }
-        if(universityImageNumber%4==0)
-        {
-            university.setImageFile("universityofcatmeme.png"); 
-        }
-        else if (universityImageNumber%4==1 || universityImageNumber%4==-1)
-        {
-            university.setImageFile("universityofbigmac.png"); 
-        }
-        else if (universityImageNumber%4 == 2 || universityImageNumber%4==-2)
-        {
-            university.setImageFile("universityofducksong.png"); 
-        }
-        else if (universityImageNumber%4 == 3 || universityImageNumber%4==-3)
-        {
-            university.setImageFile("universityoflebron.png"); 
-        }
-
-    }
 }
+
