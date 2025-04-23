f265d6c717f9d65d0bd85f78a8e7598b2544fd16
zachary <52474695+aleph-bull@users.noreply.github.com>
2025-04-23 10:55:53 -0400

    Update AdmissionsWorld.java

--- AdmissionsWorld.java
+++ AdmissionsWorld.java
@@ -156,7 +156,7 @@ public class AdmissionsWorld extends World
             if(actsSinceStartingRoller % changingNumberCooldown == 0) {
                 randomNumber = Greenfoot.getRandomNumber(101);
                 String numberString = Integer.toString(randomNumber);
-                if(randomNumber <= gpaTop) {
+                if((randomNumber <= gpaTop && !firstSequenceFinished) || (randomNumber <= gpaBot && firstSequenceFinished)) {
                     randomNumberDisplay.updateText(numberString, Color.GREEN);
                 } else {
                     randomNumberDisplay.updateText(numberString, Color.BLACK);
